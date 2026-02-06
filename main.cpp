5078ac1a0d4c832b141964f21dc7725f586a498d
shizuku002 <k024g0037@g.neec.ac.jp>
2026-02-06 16:36:38 +0900

    asdfghjkl;

--- main.cpp
+++ main.cpp
@@ -1,61 +1,55 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <time.h>
-#include <windows.h>
+#include <iostream>
+using namespace std;
 
-// 使用例
-//void PrintHelloWorld(void) { printf("HelloWorld\n"); }
-//int main(void) {
-//  // 関数ポインタを宣言
-//  void (*pfunc)();
-//
-//  pfunc = PrintHelloWorld;
-//  printf("PrintHelloWorld7 FL = %p\n", PrintHelloWorld);
-//  printf("pfunc = %p\n", *pfunc);
-//  pfunc();
-//
-//  return 0;
-//}
+class Enemy {
+public:
+    void Update(); // 状態の更新
 
-// コールバック関数のプロトタイプ宣言
-typedef void (*Callback)(int result); // 型として定義
+    void Approach(); // 接近
+    void Attack();   // 攻撃
+    void Retreat();  // 離脱
 
-// サイコロの出目を決定する関数
-int roll_dice() {
-    return rand() % 6 + 1; // ランダム関数で1～6のどれかを返す
-}
+    // 関数ポインタテーブル
+    static void (Enemy::* table[])();
 
-// 判定を行うコールバック関数
-void judge_result(int result) {
+private:
+    int index = 0; // 関数テーブルのインデックス
+};
 
+void Enemy::Approach() { cout << "敵が接近！" << endl; }
+
+void Enemy::Attack() { cout << "敵が攻撃！" << endl; }
+
+void Enemy::Retreat() { cout << "敵が離脱！" << endl; }
+
+void Enemy::Update() {
+
+    // 関数テーブルから関数を実行
+    (this->*table[index])();
+
+    cout << "次の状態に移行 (0: はい、 他: いいえ): ";
     int input;
+    cin >> input;
 
-    printf("サイコロの出目が奇数(1)か偶数(2)か入力:");
-    scanf_s("%d", &input);
-
-    printf("判定中...\n");
-
-    Sleep(3000);
-
-    if ((result % 2 == 1 && input == 1) || (result % 2 == 0 && input == 2)) {
-        printf("正解\n");
-    } else {
-        printf("不正解\n");
+    if (input == 0) {
+        index = (index + 1) % 3;
     }
-
-    printf("サイコロの目は%d\n", result);
 }
 
-// メイン関数
+// メンバ関数ポインタテーブル
+void (Enemy::* Enemy::table[])() = {
+    &Enemy::Approach, // インデックス0
+    &Enemy::Attack,   // インデックス1
+    &Enemy::Retreat   // インデックス2
+};
+
 int main() {
 
-    srand((unsigned int)time(NULL)); // 乱数の初期化
+    Enemy enemy;
 
-    int dice_result = roll_dice();
-
-    Callback callback = judge_result; // 実行したい関数のアドレス
-
-    callback(dice_result); // サイコロの値を引数にして関数ポインタから関数を実行
+    while (1) {
+        enemy.Update(); // 敵クラス実体enemyを更新
+    }
 
     return 0;
 }
