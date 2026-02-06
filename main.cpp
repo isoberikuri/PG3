41efb903bc2dbdede14e0a650d42df4cb00b917c
shizuku002 <k024g0037@g.neec.ac.jp>
2026-02-06 16:39:24 +0900

    dfghjhgfdfhnhgfg

--- main.cpp
+++ main.cpp
@@ -1,48 +1,36 @@
-#include <iostream>
+#include <cstdio>
 
-// クラステンプレート
-template <typename Type1, typename Type2> class TemplateClass {
+using namespace std;
+
+// 動物クラス（抽象クラス）
+class Animal {
 public:
-    // コンストラクタ（引数number1と2の値をメンバ変数Number1と2に退避）
-    TemplateClass(Type1 number1, Type2 number2)
-        : Number1(number1), Number2(number2) {
-    }
+	// 鳴く関数（純粋仮想関数）
+	virtual void sound() const = 0;
+};
 
-    // Min関数（小さい方を返す）
-    auto Min() {
-        if (Number1 < Number2)
-            return static_cast<decltype(Number1 + Number2)>(Number1);
-        else
-            return static_cast<decltype(Number1 + Number2)>(Number2);
-    }
+// 犬クラス（Animalを継承）
+class Dog : public Animal {
+public:
+	void sound() const override { printf("イヌ鳴き声：ワン\n"); }
+};
 
-private:
-    Type1 Number1; // number1の値を格納する変数1
-    Type2 Number2; // number2の値を格納する変数2
+// 猫クラス（Animalを継承）
+class Cat : public Animal {
+public:
+	void sound() const override { printf("ネコ鳴き声：ニャー\n"); }
 };
 
 int main() {
+	// 動物クラスのポインタ
+	Animal* animal1 = new Dog();
+	Animal* animal2 = new Cat();
 
-    // テンプレートから6種類のクラスを定義
-    TemplateClass<int, float> intFloatTemplate(100, 50.0f);          // ①
-    TemplateClass<int, double> intDoubleTemplate(80, 20.0);          // ②
-    TemplateClass<float, int> floatIntTemplate(2.0f, 18);            // ③
-    TemplateClass<float, double> floatDoubleTemplate(11.0f, 3.5);    // ④
-    TemplateClass<double, int> doubleIntTemplate(234.0, 123);        // ⑤
-    TemplateClass<double, float> doubleFloatTemplate(400.1, 500.2f); // ⑥
+	animal1->sound();
+	animal2->sound();
 
-    std::cout << "int(100)とfloat(50.0f)を比べて小さい数字を返す："
-        << intFloatTemplate.Min() << std::endl;
-    std::cout << "int(80)とdouble(20.0)を比べて小さい数字を返す："
-        << intDoubleTemplate.Min() << std::endl;
-    std::cout << "float(2.0f)とint(18)を比べて小さい数字を返す："
-        << floatIntTemplate.Min() << std::endl;
-    std::cout << "float(11.0f)とdouble(3.5)を比べて小さい数字を返す："
-        << floatDoubleTemplate.Min() << std::endl;
-    std::cout << "double(234.0)とint(123)を比べて小さい数字を返す："
-        << doubleIntTemplate.Min() << std::endl;
-    std::cout << "double(400.1)とfloat(500.2)を比べて小さい数字を返す："
-        << doubleFloatTemplate.Min() << std::endl;
+	delete animal1;
+	delete animal2;
 
-    return 0;
+	return 0;
 }
