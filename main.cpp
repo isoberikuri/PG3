61d534d233d035917302f5e7ae3102599d839f3f
shizuku002 <k024g0037@g.neec.ac.jp>
2026-02-12 22:32:33 +0900

    asdfghjkjhgfd

--- main.cpp
+++ main.cpp
@@ -1,21 +1,80 @@
+#include <chrono>
+#include <fstream>
 #include <iostream>
+#include <mutex>
+#include <sstream>
 #include <thread>
+#include <vector>
 
 using namespace std;
 
-void PrintThread(uint32_t num) { cout << "thread" << num << endl; }
+vector<vector<int>> mapData;
+bool loaded = false;
+
+mutex mapMutex;
+bool running = true;
+
+void LoadCSV() {
+    cout << "[BG] CSV Load Start" << endl;
+
+    ifstream file("map.csv");
+    string line;
+
+    vector<vector<int>> tempMap;
+
+    while (getline(file, line)) {
+        stringstream ss(line);
+        string value;
+        vector<int> row;
+
+        while (getline(ss, value, ',')) {
+            row.push_back(stoi(value));
+        }
+        tempMap.push_back(row);
+    }
+
+    {
+        lock_guard<mutex> lock(mapMutex);
+        mapData = tempMap;
+        loaded = true;
+    }
+
+    cout << "[BG] CSV Load Complete" << endl;
+}
+
+void MainLoop() {
+    while (running) {
+        if (loaded) {
+            lock_guard<mutex> lock(mapMutex);
+
+            system("cls");
+            cout << "=== MAP ===" << endl;
+
+            for (auto& row : mapData) {
+                for (int chip : row) {
+                    cout << chip << " ";
+                }
+                cout << endl;
+            }
+        } else {
+            cout << "Loading..." << endl;
+        }
+
+        this_thread::sleep_for(chrono::milliseconds(500));
+    }
+}
 
 int main() {
 
-	// マルチスレッドではある
-	thread t1(PrintThread, 1);
-	t1.join();
+    thread bgThread(LoadCSV);
+    thread mainThread(MainLoop);
 
-	thread t2(PrintThread, 2);
-	t2.join();
+    this_thread::sleep_for(chrono::seconds(5));
+    running = false;
 
-	thread t3(PrintThread, 3);
-	t3.join();
+    bgThread.join();
+    mainThread.join();
 
-	return 0;
+    cout << "終了" << endl;
+    return 0;
 }
